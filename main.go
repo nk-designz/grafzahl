3a6a3ef245678aa9b11481382aa77e98390f9f6f
robbmue <robinmueller77@gmail.com>
2020-11-25 20:38:11 +0100

    ADD Everything

--- main.go
+++ main.go
@@ -15,11 +15,12 @@ import (
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"github.com/qiangxue/go-env"
 	log "github.com/sirupsen/logrus"
+	//"github.com/docker/distribution/manifest/schema2"
 	"gopkg.in/yaml.v3"
 )
 
 const (
-	probeRepo = "me/grafzahl"
+	probeRepo = "nicokahlert/grafzahl"
 
 	lOptFlag     = iota
 	lOptEnv
@@ -40,6 +41,12 @@ var (
 			Help: "The maximal pulls for this account.",
 		},
 	)
+	images = prometheus.NewGauge(
+		prometheus.GaugeOpts{
+			Name: "last_image_update_time",
+			Help: "The last time an image available in this repo was updated.",
+		},
+	)
 	client = &http.Client{
 		Transport: &http.Transport{
 			TLSClientConfig: &tls.Config{InsecureSkipVerify : true},
@@ -54,11 +61,41 @@ type Exporter struct {
 	Token     string
 	Limit     int
 	Remaining int
+	Images    []int
+	Catalogue []*Repository
+
 }
 
 // TokenResp from Dockerhub
 type TokenResp struct {
 	Token string `json:"token"`
+	Details string `json:"details"`
+}
+
+// CatalogueResponse with available images in repo
+type CatalogueResponse struct {
+	Repositories []string `json:"repositories"`
+}
+
+//TagResponse F
+type TagResponse struct {
+	Tags []Tag `json:"tags"`
+}
+
+//Tag F
+type Tag string
+
+//Repository F
+type Repository struct {
+	Name string
+	Tags map[Tag]int64
+}
+
+// SadManifesto F
+type SadManifesto struct {
+	History []struct {
+		V1Compatibility string `json:"v1Compatibility"`
+	} `json:"history"`
 }
 
 // GetToken from Dockerhub
@@ -87,6 +124,7 @@ func (e *Exporter) GetToken() error {
 		return err
 	}
 	if tokenResp.Token == "" {
+		log.Warn(tokenResp.Details)
 		log.Fatal("No token!!! >:(")
 	}
 	e.Token = tokenResp.Token
@@ -94,7 +132,7 @@ func (e *Exporter) GetToken() error {
 	return nil
 }
 
-// Request your Limits and Remainings
+// Request your Limits and Remaining
 func (e *Exporter) Request() error {
 	req, err := http.NewRequest("HEAD", fmt.Sprintf("https://registry-1.docker.io/v2/%s/manifests/latest", probeRepo), nil)
 	if err != nil {
@@ -120,6 +158,107 @@ func (e *Exporter) Request() error {
 	e.Limit, err = strconv.Atoi(strings.Split(rawLimit, ";")[0])
 	e.Remaining, err = strconv.Atoi(strings.Split(rawRemaining, ";")[0])
 	return err
+}
+
+// GetCatalogue checks the available Images
+func (e *Exporter) GetCatalogue() error  {
+	resp, err := http.Get("http://localhost:5000/v2/_catalog")
+	if err != nil {
+		log.Warn(err)
+		return err
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Warn(err)
+		return err
+	}
+	var catalogueResponse CatalogueResponse
+	if err := json.Unmarshal(body, &catalogueResponse); err != nil {
+		log.Warn(err)
+		return err
+	}
+	for _,c := range(catalogueResponse.Repositories){
+		e.Catalogue = append(e.Catalogue, &Repository{
+			Name: c,
+			Tags: make(map[Tag]int64),
+		})
+	}
+	return e.GetTags()
+}
+
+// GetTags F
+func (e *Exporter) GetTags() error  {
+	for _,r := range(e.Catalogue){
+		resp, err := http.Get(fmt.Sprintf("http://localhost:5000/v2/%s/tags/list", r.Name ))
+		if err != nil {
+			log.Warn(err)
+			return err
+		}
+		defer resp.Body.Close() //TODO auslagern!!!!!!
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			log.Warn(err)
+			return err
+		}
+		var tagResponse TagResponse
+		if err := json.Unmarshal(body, &tagResponse); err != nil {
+			log.Warn(err)
+			return err
+		}
+		for _,t := range(tagResponse.Tags){
+			r.Tags[t]=0
+		}
+		r.GetTime()
+	}
+	return nil
+}
+
+func httpUnmarshall(url string, v interface{}) error {
+	resp, err := http.Get(url)
+		if err != nil {
+			log.Warn(err)
+			return err
+		}
+		defer resp.Body.Close()
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			log.Warn(err)
+			return err
+		}
+		return json.Unmarshal(body, v)
+}
+
+// GetTime F
+func (r *Repository) GetTime() error {
+	for tag := range(r.Tags) {
+		var manifest SadManifesto
+		if err := httpUnmarshall(
+			fmt.Sprintf(
+				"http://localhost:5000/v2/%s/manifests/%s",
+				r.Name,
+				tag,
+			),
+			&manifest,
+		); err != nil {
+			log.Warn(err)
+			return err
+		}
+		compString := manifest.History[len(manifest.History)-1].V1Compatibility
+		var comp map[string]interface{}
+		if err := json.Unmarshal([]byte(compString), &comp); err != nil {
+			log.Warn(err)
+			return err
+		}
+		created, err := time.Parse("2006-01-02T15:04:05.000000000Z", comp["created"].(string))
+		created_u64 := created.Unix
+		if err != nil {
+			log.Warn(err)
+			return err
+		}
+		r.Tags[tag] = created_u64()
+	}
+	return nil
 }
 
 // RenewTokenEvery n hours
@@ -157,13 +296,30 @@ func (e *Exporter) RenewLimitValuesEvery(n time.Duration) {
 	}()
 }
 
+// RenewImagesEvery renews images available in Registry every n seconds
+func (e *Exporter)RenewImagesEvery(n time.Duration)  {
+	go func () {
+		for {
+			err := e.GetCatalogue()
+			if err != nil {
+				log.Warn(err)
+			}
+			images.Set(3)
+			time.Sleep(n)
+			
+		}
+	}()
+}
+
 // Run the Exporter
 func (e *Exporter) Run() {
 	log.Info("Starting exporter on localhost:6969/metrics")
 	prometheus.MustRegister(limit)
 	prometheus.MustRegister(remaining)
+	prometheus.MustRegister(images)
 	e.RenewTokenEvery(2 * time.Hour)
 	time.Sleep(3 * time.Second)
+	e.RenewImagesEvery(3 * time.Second)
 	e.RenewLimitValuesEvery(3 * time.Second)
 	http.Handle("/metrics", promhttp.Handler())
 	log.Info("Endpoint ready")
